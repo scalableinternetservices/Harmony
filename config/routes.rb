7690a0183194e5f77e2efc047933187cd6304a18
chang <tariad@outlook.com>
2021-11-04 17:11:37 -0700

    add channels and view channels

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/channels", to: "channels#index"
+  # get "/channels/:id", to: "channels#show"
+  # get "/channels/new", to: "channels#new"
+  # post "/channels", to: "channels#create"
+
+  resources :channels
 end
