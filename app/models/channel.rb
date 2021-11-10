cb8eafded46eb59741787320e99616f0cdb27a91
Abby Wysopal <abbywysopal@umail.ucsb.edu>
2021-11-10 14:52:14 -0800

    set up associations for channel to setup notification model

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,5 @@
 class Channel < ApplicationRecord
   has_many :messages, dependent: :destroy
+  has_many :users, # through: :messages #this will set users of a channel to those who have sent a message to that channel
   validates :name, presence: true
 end
