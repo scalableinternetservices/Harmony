d888c18b9ccc6ac676723d986bc93e11bede5966
Abby Wysopal <abbywysopal@umail.ucsb.edu>
2021-11-10 15:33:32 -0800

    assossication between user and channel

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,5 @@
 class Channel < ApplicationRecord
   has_many :messages, dependent: :destroy
+  has_many :users#, through :messages #this will have users in channel be defined by a user who has posted a message to that channel
   validates :name, presence: true
 end
