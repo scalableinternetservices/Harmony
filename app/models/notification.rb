d246b15ed41e13520865b3fd15e949f75751f130
Abby Wysopal <abbywysopal@umail.ucsb.edu>
2021-11-10 15:58:54 -0800

    sytax error fixed

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ApplicationRecord
-    belongs_to: :recipient, class_name: "User"
-    belongs_to: :actor, class_name: "User"
-    belongs_to: :notifiable, polymorphic: true
+    belongs_to :recipient, class_name: "User"
+    belongs_to :actor, class_name: "User"
+    belongs_to :notifiable, polymorphic: true
 end
