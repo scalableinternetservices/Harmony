ab434bcef0dac2c5e3ccdcd4dbcc98afc04fc821
Abby Wysopal <abbywysopal@umail.ucsb.edu>
2021-11-10 21:15:08 -0800

    added notification routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get "/channels", to: "channels#index"
-  # get "/channels/:id", to: "channels#show"
-  # get "/channels/new", to: "channels#new"
-  # post "/channels", to: "channels#create"
   root "channels#index"
   resources :messages
   resources :users
   resources :channels do
     resources :messages
+  end
+  resources :notifications do
+    collection do
+      post :mark_as_read
+    end
   end
   resources :sessions
 end
