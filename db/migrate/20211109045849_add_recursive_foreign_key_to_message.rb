131ad9f14a21f39d2b69b23373eebf3d3c7b089a
ViVidam <1325507519@qq.com>
2021-11-11 12:41:04 -0800

    remove duplicated migrate

--- db/migrate/20211109045849_add_recursive_foreign_key_to_message.rb
+++
@@ -1,7 +0,0 @@
-class AddRecursiveForeignKeyToMessage < ActiveRecord::Migration[6.1]
-  def change
-    change_table :messages do |t|
-      t.references :parent_message, index:true, foreign_key: { to_table: :messages }
-    end
-  end
-end
