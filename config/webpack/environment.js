ddc8242407ee09368b3b3270b256a243f4ab4a8c
Abby Wysopal <abbywysopal@umail.ucsb.edu>
2021-11-12 10:36:43 -0800

    removing jQuery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,2 @@
 const { environment } = require('@rails/webpacker')
-
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery'
-}))
-
 module.exports = environment
