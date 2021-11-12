61e927fb7231a1a9e6f8eb9ac7ec3c66880fd6b4
ViVidam <1325507519@qq.com>
2021-11-11 20:33:14 -0800

    fix a bug that could cause failure in seed

--- config/initializers/seed.rb
+++ config/initializers/seed.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if(!User.find_by(id:1))
+	Message.delete_all
+	Message.connection.execute('ALTER SEQUENCE messages_id_seq RESTART WITH 1')
 	User.delete_all
 	User.connection.execute('ALTER SEQUENCE users_id_seq RESTART WITH 1')
 	User.create(username: 'guest', firstname: 'guest',password: 'guest', lastname: 'guest', age: 99, gender: 'M', location: 'Goleta')
