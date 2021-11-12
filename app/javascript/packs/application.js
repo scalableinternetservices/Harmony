66c9222e841112182edf2bb654624a7f2fc1567d
Abby Wysopal <abbywysopal@umail.ucsb.edu>
2021-11-11 18:43:14 -0800

    added more dependcies for bootstrap and jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require_tree 
+//= require_self
+//= require jquery
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
+
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 
+require("jquery")
 Rails.start()
 ActiveStorage.start()
-
 
 
 
