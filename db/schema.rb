ea51729d34c9067d6f060126f7994de13801f4df
Abby Wysopal <abbywysopal@umail.ucsb.edu>
2021-11-14 20:40:21 -0800

    message edit and reply

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_193337) do
+ActiveRecord::Schema.define(version: 2021_11_14_204650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,13 +21,22 @@ ActiveRecord::Schema.define(version: 2021_11_10_193337) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "employees", force: :cascade do |t|
+    t.bigint "parent_message_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["parent_message_id"], name: "index_employees_on_parent_message_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "channel_id"
     t.bigint "user_id"
+    t.bigint "parent_message_id"
     t.index ["channel_id"], name: "index_messages_on_channel_id"
+    t.index ["parent_message_id"], name: "index_messages_on_parent_message_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -56,6 +65,8 @@ ActiveRecord::Schema.define(version: 2021_11_10_193337) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "employees", "employees", column: "parent_message_id"
   add_foreign_key "messages", "channels"
+  add_foreign_key "messages", "messages", column: "parent_message_id"
   add_foreign_key "messages", "users"
 end
