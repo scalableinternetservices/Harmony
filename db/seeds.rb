da10a305d9e0775a812b8691a6cb59b0d322c69f
ViVidam <1325507519@qq.com>
2021-11-16 23:50:06 -0800

    fix bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ end
 
 
 if(!User.find_by(id:1))
-	Messages.delete_all()
+	Message.delete_all()
 	User.delete_all
 	User.connection.execute('ALTER SEQUENCE users_id_seq RESTART WITH 1')
 	User.create(username: 'guest', firstname: 'guest',password: 'guest', lastname: 'guest', age: 99, gender: 'M', location: 'Goleta')
@@ -26,8 +26,8 @@ Message.connection.execute('ALTER SEQUENCE messages_id_seq RESTART WITH 1')
 Channel.connection.execute('ALTER SEQUENCE channels_id_seq RESTART WITH 1')
 #change these two var if you need
 #the number too large might need huge number to start up the app
-@channelMaxNum = 10
-@messageMaxNuminChannel = 30
+@channelMaxNum = 3
+@messageMaxNuminChannel = 5
 
 #these are the generate process
 @channelNum = rand(@channelMaxNum)
