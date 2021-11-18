3fd87d7b5041ba5a3f31322168524070cfd5d51b
ViVidam <1325507519@qq.com>
2021-11-18 12:36:44 -0800

    test deployment on create

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,13 +28,11 @@ Channel.connection.execute('ALTER SEQUENCE channels_id_seq RESTART WITH 1')
 @channelNum = rand(@channelMaxNum)
 @i=0
 until @i>@channelNum do
-	tmp = Channel.new(name:generate_string(5))
-	tmp.save()
+	tmp = Channel.create(name:generate_string(5))
 	@messageNum = rand(@messageMaxNuminChannel)
 	@j = 0
 	until @j>@messageNum
-		tmpMessage = tmp.messages.build(content:generate_string(10),user_id:1)
-		tmpMessage.save()
+		tmpMessage = tmp.messages.create(content:generate_string(10),user_id:1)
 		@j+=1
 	end
 	@i+=1
