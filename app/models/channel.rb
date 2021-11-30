9bdf5ceb5a7b628e5e40e526ce02d0cf403044b8
rohanbh <brohan52@gmail.com>
2021-11-29 17:54:13 -0800

    Add caching for users list in channel

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,4 +2,10 @@ class Channel < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :users, through: :messages #this will have users in channel be defined by a user who has posted a message to that channel
   validates :name, presence: true
+
+  def uniq_users
+    Rails.cache.fetch(cache_key_with_version, expires_in: 15.seconds) do
+      self.users.uniq
+    end
+  end
 end
